df8c10f515d0f08973b0ac9cd4f4bc68c596beee
Tominiyi Ajayi <tominiyiajayi@gmail.com>
2019-01-26 23:58:46 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,31 @@
-//define functions here
+function getIt(){
+  $("p").on("click", function() {
+    alert("Hey!");
+    })
+  }
+
+
+function frameIt(){
+  $("img").on("load", function(){
+    $("img").addClass("tasty")
+  })
+}
+
+function pressIt(){
+  $("#typing").on("keydown", function(key){
+    if (key.which == 71){
+      alert("You pressed 'g'!");
+    }
+  })
+}
+
+function submitIt(){
+  $("form").on("submit", function(e){
+      alert("Your form is going to be submitted now.");
+      return;
+    })
+}
 
 $(document).ready(function(){
-
-// call functions here
 
 });
